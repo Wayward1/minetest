23946efb05a97b0cbe432489011f5aa8c454c044
Wayward One <calmthefirewithin@gmail.com>
2016-11-24 16:41:19 -0500

    Change default leaves style to "opaque" on Android

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -352,6 +352,7 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("TMPFolder","/sdcard/" PROJECT_NAME_C "/tmp/");
 	settings->setDefault("touchscreen_threshold","20");
 	settings->setDefault("smooth_lighting", "false");
+	settings->setDefault("leaves_style", "opaque");
 	settings->setDefault("max_simultaneous_block_sends_per_client", "3");
 	settings->setDefault("emergequeue_limit_diskonly", "8");
 	settings->setDefault("emergequeue_limit_generate", "8");
