9804ae9e1a1e0647f825bf974a0a30153e552710
Wayward One <calmthefirewithin@gmail.com>
2018-02-19 14:43:07 -0500

    Implement immersive mode
    This allows Minetest to automatically hide the on-screen menu bar present on some devices, therefore allowing Minetest to utilize the entire screen. This was taken from #4046 and edited to fit the current code base.

--- build/android/src/main/java/net.minetest.minetest/MtNativeActivity.java
+++ build/android/src/main/java/net.minetest.minetest/MtNativeActivity.java
@@ -3,8 +3,10 @@ package net.minetest.minetest;
 import android.app.NativeActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Build;
 import android.util.Log;
 import android.view.WindowManager;
+import android.view.View;
 
 public class MtNativeActivity extends NativeActivity {
 	@Override
@@ -13,7 +15,13 @@ public class MtNativeActivity extends NativeActivity {
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		m_MessagReturnCode = -1;
 		m_MessageReturnValue = "";
-
+		// System UI flags have to be set in onCreate, because
+		// onWindowFocusChanged apparently runs after the native
+		// code has retreived the window dimensions, and therefore
+		// everything on the screen is offset when
+		// LAYOUT_HIDE_NAVIGATION is set.
+		setImmersive();
+		
 	}
 
 	@Override
@@ -41,6 +49,33 @@ public class MtNativeActivity extends NativeActivity {
 		m_MessagReturnCode   = -1;
 	}
 
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		if (hasFocus)
+			setImmersive();
+	}
+
+	private void setImmersive() {
+		if (Build.VERSION.SDK_INT < 16)
+			return;
+
+		// Hide status bar and other inessential UI items
+		int visibility = View.SYSTEM_UI_FLAG_FULLSCREEN |
+			View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+			View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+		if (Build.VERSION.SDK_INT >= 19) {
+			// HIDE_NAVIGATION is available since API 14, but the
+			// navigation re-appears on any touch events when not
+			// paired with IMMERSIVE (API 19), making it essentially
+			// useless alone.
+			visibility |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+				View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+				View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+		}
+		getWindow().getDecorView().setSystemUiVisibility(visibility);
+	}	
+	
 	public static native void putMessageBoxResult(String text);
 
 	/* ugly code to workaround putMessageBoxResult not beeing found */
